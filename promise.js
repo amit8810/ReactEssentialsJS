3f2fb4fae4bed365fd6cf4ec31730529ad1a789f
amit8810 <ac446643@gmail.com>
2024-02-16 22:07:34 +0530

    promise more detail added

--- promise.js
+++ promise.js
@@ -6,40 +6,56 @@
  * * Rejected: operation was failed, and promise has a resaon for failure.
  */
 
-
 // Function to simulate an asynchronous operation
 function asyncOperation() {
+  return new Promise(function (resolve, reject) {
+    // Simulating an asynchronous task (e.g., API call)
+    setTimeout(function () {
+      const success = Math.random() < 0.8;
 
-    return new Promise(function (resolve, reject) {
-        // Simulating an asynchronous task (e.g., API call)
-        setTimeout(function() {
-            const success = Math.random() < 0.8;
-
-            if(success) {
-                resolve("Operation completed successfully");
-            } else {
-                reject("Operation failed");
-            }
-        }, 1000);
-    });
+      if (success) {
+        resolve("Operation completed successfully");
+      } else {
+        reject("Operation failed");
+      }
+    }, 1000);
+  });
 }
 
 // Using the Promise
 asyncOperation()
- .then(function(result) {
+  .then(function (result) {
     console.log("Success: ", result);
- })
- .catch(function(error) {
+  })
+  .catch(function (error) {
     console.error("Error: ", error);
- });
+  });
 
+// resolve - conected hai .then() se; jo resolve mein daalke bhejoge wo .then mein jayega
+// reject - connected hai .catch() se; jo reject mein daalke bhejoge wo .catch mein jayega
 
 // real-world example
 function fetchDataFromApi(url) {
-    
-    return new Promise(() => {
-
-    })
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            fetch(url)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        reject(new Error('Request failed'));
+                    }
+                })
+                .then(data => resolve(data))
+                .catch(error => reject(error));
+        }, 2000);
+    });
 }
 
-
+fetchDataFromApi('https://api.github.com/users/amit8810')
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(error) {
+        console.log("Error", error);
+    });
